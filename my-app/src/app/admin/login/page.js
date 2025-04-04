74ef1f6625b836aa051a151ec995cfa98151c31f
ashishdevx <ashish.santani@devxconsultancy.com>
2025-04-05 04:39:23 +0530

    fixes

---
+++ my-app/src/app/admin/login/page.js
@@ -0,0 +1,12 @@
+import AdminLogin from "@/components/AdminLogin";
+
+export default function AdminLoginPage() {
+  //call login API admin
+  // if fail redirect to login
+
+  // if success redirect to admin panel
+
+  //
+
+  return <AdminLogin />;
+}
